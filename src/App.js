034a0d192ca776e4fa2281a27356aeddd43099dd
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-28 11:32:37 -0600

    add structure for App.js, folders for components and installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/pages/Home/Home";
+import Game from "./components/pages/Game/Game";
+import Story from "./components/pages/Story/Story";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <></>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/game" element={<Game />} />
+          <Route exact path="/story" element={<Story />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h4>Mia & Holly Productions</h4>
+    </footer>
+  );
+}
+
+export default Footer;
