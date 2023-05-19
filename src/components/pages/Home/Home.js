13844a46c4501fe5f5989bd0a792b4356ce5bacb
miamauro <miiamauro@outlook.com>
2023-05-19 13:53:15 -0500

    added color theme to game page and story page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -6,15 +6,18 @@ const Home = () => {
   const navigate = useNavigate();
   const startGame = (event) => {
     navigate("/game");
+    window.scrollTo(0, 0);
   };
   return (
     <div className="container vh-75 intro">
       <p>
-        For all the <span id="kids-color">90s kids</span>, this game will bring
-        you back to <span id="sleepovers">sleepovers</span> with your friends, deciding your future with a
-        paper, pencil, and the process of elimination. This version of MASH
-        brings the <span id="nostalgic">nostalgic</span> experience right to your fingertips! Proceed
-        through the prompts and at the end, the game will determine your <span id="fate">fate</span>!
+        For all the <span id="kids-color">90's kids</span>, this game will bring
+        you back to <span id="sleepovers">sleepovers</span> with your friends,
+        deciding your future with a piece of paper, a pencil, and the process of
+        elimination. This version of MASH brings the{" "}
+        <span id="nostalgic">nostalgic</span> experience right to your
+        fingertips! Proceed through the prompts and at the end, the game will
+        determine your <span id="fate">fate</span>!
       </p>
       <button
         type="submit"
