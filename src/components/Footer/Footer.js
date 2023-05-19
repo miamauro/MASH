5f4090acae2241b333e4ff7235e10fe675e80844
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-05-19 12:51:03 -0600

    add footer logo and update footer components :

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../Footer/Footer.css";
+import mhlogo from "../images/MHProductions.png";
 
 function Footer() {
   return (
     <footer className="footer">
-      <h4>Mia & Holly Productions</h4>
+      {/* <h4>Mia & Holly Productions</h4> */}
+      <img src={mhlogo} alt="mh logo"></img>
     </footer>
   );
 }
